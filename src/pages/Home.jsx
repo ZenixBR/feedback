de89bf1037b8f4e79f5b8f06fd786a564bb3cdd7
Vinicius Viera <viniciusviera301@gmail.com>
2024-06-10 01:09:00 +0000

    style: estilizacao completa

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
-import { ButtonContainer, CardContainer, IconContainer } from "./Home.styles";
+import { ButtonContainer, CardContainer, IconContainer, ResultContainer } from "./Home.styles";
 
 import iconStartImg from "../assets/icon-star.svg"
+import thankYouImg from "../assets/illustration-thank-you.svg"
 
 export function Home() {
+  let aparecerResultado = true
+  
   return (
+    aparecerResultado === false ? (
     <CardContainer>
       <IconContainer>
         <img src={iconStartImg} alt="icone de estrela" />
@@ -23,5 +27,17 @@ export function Home() {
 
       <button>Enviar</button>
     </CardContainer>
-  )
+    ) : (
+      <CardContainer>
+        <img src={thankYouImg} alt="imagem de agradecimento" />
+
+        <ResultContainer>
+          <p>Você selecionou {} de 5</p>
+        </ResultContainer>
+
+        <h1>Obrigado!</h1>
+
+        <p>Agradecemos por dedicar um momento para nos avaliar. Se precisar de mais suporte, não hesite em entrar em contato!</p>
+      </CardContainer>)
+    )
 }
